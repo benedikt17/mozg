2e93bf32399928e33bdbe46a7b976cfa0cd0f07c
benedikt17 <benediktov.andrey@gmail.com>
2026-08-09 23:23:49 +0500

    refactor: drop legacy Canvas state export

--- src/prototype/desktop-state.ts
+++ src/prototype/desktop-state.ts
@@ -4,5 +4,4 @@ export * from "@/prototype/state/selectors";
 export * from "@/prototype/state/overview-state";
 export * from "@/prototype/state/knowledge-state";
 export * from "@/prototype/state/tasks-state";
-export * from "@/prototype/state/canvases-state";
 export * from "@/prototype/state/inbox-state";
